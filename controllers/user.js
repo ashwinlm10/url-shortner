83235c2e15beb2f9e8b5e94c4a476958f89691d2
Ashwin <ashwinlmashwin648@gmail.com>
2025-08-15 18:48:34 +0530

    Initial commit

---
+++ controllers/user.js
@@ -0,0 +1,34 @@
+const {v4: uuidv4} = require('uuid')
+const User = require("../models/user")
+const {setUser} = require("../service/auth")
+
+async function handleUserSignup(req,res) {
+    const { name,email,password} = req.body
+    await User.create({
+        name,
+        email,
+        password,
+    })
+     return res.redirect('/')
+}
+
+async function handleUserLogin(req,res) {
+    const { email,password} = req.body
+    const user = await User.findOne({email,password})
+    // console.log("User",user)
+    if (!user)
+        return res.render("login",{
+    error: "invalid Username or Possword"
+})
+
+    const sessionid = uuidv4()
+    setUser(sessionid,user)
+    res.cookie("uid",sessionid,{httpOnly: true})
+    return res.redirect('/')
+}
+
+
+module.exports = {
+    handleUserSignup,
+    handleUserLogin
+}
